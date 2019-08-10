82ee9ee7f0fe1e7d49259c8b48b3dd4bd0b5a9a6
Anwar Numa <anwarnuma@hotmail.com>
2019-08-10 16:51:52 -0400

    Added data persistence using user defaults

--- todoey/TodoListViewController.swift
+++ todoey/TodoListViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggs", "Destory Demogorgon"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     //MARK: - Table View Datasource Methods
@@ -53,6 +58,9 @@ class TodoListViewController: UITableViewController {
             //add what happens when its pressed
             //print(textField.text)
             self.itemArray.append(textField.text ?? "New Item")
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
