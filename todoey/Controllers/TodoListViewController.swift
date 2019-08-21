15cd26f587ee44766c6f4fa750eba31b3b7bfaf3
Anwar Numa <anwarnuma@hotmail.com>
2019-08-20 21:11:10 -0400

    Add gradient color scheme using chameleon framework

--- todoey/Controllers/TodoListViewController.swift
+++ todoey/Controllers/TodoListViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class TodoListViewController: SwipeTableViewController{
     
@@ -25,7 +26,8 @@ class TodoListViewController: SwipeTableViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadItems()
+        tableView.separatorStyle = .none
+        
         
     }
 
@@ -38,6 +40,12 @@ class TodoListViewController: SwipeTableViewController{
             cell.textLabel?.text = item.title
         
             cell.accessoryType = item.done ? .checkmark : .none
+            if let color =  UIColor(hexString: selectedCategory!.color)?.darken(byPercentage: CGFloat(indexPath.row) / CGFloat(todoItems!.count)){
+                cell.backgroundColor = color
+                cell.textLabel?.textColor = ContrastColorOf(color, returnFlat: true)
+            }
+            
+            
 
         }else{
             cell.textLabel?.text = "No Items Added"
@@ -117,6 +125,7 @@ class TodoListViewController: SwipeTableViewController{
                 try self.realm.write {
                     self.realm.delete(itemForDeletion)
                 }
+
             }catch{
                 print("Error deleting the data with Error: \(error)")
             }
